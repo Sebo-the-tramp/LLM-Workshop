9db9afc3e0d23bc0099bfe30b221e7a3ba684840
Sebastian Cavada <sebastian.cavada.lab@gmail.com>
2024-05-31 14:22:55 +0200

    refactor ahahah

--- 6_Module/question_answering_gradio_app/run_scrapy.sh
+++
@@ -1,3 +0,0 @@
-scrapy startproject doc_scraper
-cd doc_scraper
-scrapy genspider hf_docs huggingface.co/docs/peft
--- personal_copilot/dataset_generation/prepare_hf_dataset.py
+++ personal_copilot/dataset_generation/prepare_hf_dataset.py
@@ -13,36 +13,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import pandas as pd
-import gzip
-import json
 from datasets import Dataset
 
-DATAFOLDER = "hf_stack"
-HF_DATASET_NAME = "smangrul/hug_stack"
-
-
-def load_gzip_jsonl(file_path):
-    data = []
-    with gzip.open(file_path, "rt", encoding="utf-8") as f:
-        for line in f:
-            data.append(json.loads(line))
-    return data
-
+DATAFOLDER = "/Users/sebastiancavada/Documents/scsv/internship/internship_CVI2/finetuning/representations/token/data"
+HF_DATASET_NAME = "sebothetramp/sketch_token_small"
 
 def create_hf_dataset():
-    df = None
-    for file in os.listdir(DATAFOLDER):
-        data = load_gzip_jsonl(os.path.join(DATAFOLDER, file))
-        if df is None:
-            df = pd.DataFrame(data)
-        else:
-            df = pd.concat([df, pd.DataFrame(data)])
+    df = pd.read_parquet(DATAFOLDER + "/sketch.parquet")
 
     dataset = Dataset.from_pandas(df)
-    dataset.push_to_hub(HF_DATASET_NAME, private=False)
-
+    dataset.save_to_disk(DATAFOLDER + "/sketch_hf")
+    # dataset.push_to_hub(HF_DATASET_NAME, private=False)
 
 if __name__ == "__main__":
     create_hf_dataset()
